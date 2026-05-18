f98437bcfff3a3114c3dd74d6a2adcbe0849d70f
Arthur D'Avila <arthurdavila879@gmail.com>
2026-05-17 23:11:09 -0300

    Update API_URL to production endpoint

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ const App = {
 };
 
 // ===== API HELPERS =====
-const API_URL = 'http://localhost:3000';
+const API_URL = 'https://inova-x6vv.onrender.com/';
 
 async function apiFetch(endpoint, options = {}) {
   const token = localStorage.getItem('inova_token');
